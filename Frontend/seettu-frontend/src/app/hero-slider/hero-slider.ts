96bcf01484ff7b8c76ba587f915061e4ada037a8
DilmikaSahan <153601909+DilmikaSahan@users.noreply.github.com>
2025-07-16 16:03:21 +0530

    craete nav bar and add slide show on home page

---
+++ Frontend/seettu-frontend/src/app/hero-slider/hero-slider.ts
@@ -0,0 +1,30 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hero-slider',
+  imports: [CommonModule],
+  templateUrl: './hero-slider.html',
+  styleUrl: './hero-slider.scss'
+})
+export class HeroSlider implements OnInit,OnDestroy {
+  images =[
+
+  ];
+  currentIndex=0;
+  intervalID?:any;
+
+  ngOnInit(): void {
+    this.startSlideshow();
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.intervalID)
+  }
+  startSlideshow(){
+    this.intervalID=setInterval(()=>{
+    this.currentIndex=(this.currentIndex+1)%this.images.length;
+    },3000);
+  }
+
+
+}
--- Frontend/seettu-frontend/src/app/modules/home/home.ts
+++ Frontend/seettu-frontend/src/app/modules/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbar } from "@angular/material/toolbar";
+import { HeroSlider } from "../../hero-slider/hero-slider";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatToolbar, HeroSlider],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
