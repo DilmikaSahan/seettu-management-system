fa2d17c9d5a995014583e07df986d5a80a69cea3
DilmikaSahan <153601909+DilmikaSahan@users.noreply.github.com>
2025-07-27 12:56:08 +0530

    add business man and customer cards

--- Frontend/seettu-frontend/src/app/modules/home/home.ts
+++ Frontend/seettu-frontend/src/app/modules/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from "@angular/material/toolbar";
 import { HeroSlider } from "../../hero-slider/hero-slider";
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [MatToolbar, HeroSlider],
+  imports: [MatToolbar, HeroSlider, MatButtonModule, MatCardModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
